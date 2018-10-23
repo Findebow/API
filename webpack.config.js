eb806cec0db9beb835b07e016670bf5cb95d7d32
Findebow <finnbogih17@ru.is>
2018-10-23 06:17:48 +0000

    lala

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('HtmlWebpackPlugin');
+const CleanWebpackPlugin = require('CleanWebpackPlugin');
+
 module.exports = {
+    devServer: {
+        port: 3000,
+        open: true,
+        proxy: {
+            "/api" : "http://localhost:8080"
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(dist)
+    ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Greeting page'
+        })
+    ],
     entry: './src/client/index.js',
     output: {
         filename: 'main.js',
